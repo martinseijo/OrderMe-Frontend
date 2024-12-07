68058dcbcdf1c640179970af30334170caf4cc53
Martín Seijo <martinseijoperez@gmail.com>
2024-12-07 13:54:32 +0100

    Se cambia a la ruta de PRO

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://192.168.1.138:8080',
+    baseURL: 'https://orderme.onrender.com',
 });
 
 api.interceptors.request.use(
